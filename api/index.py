6d76975b3e5d717e14efe4517a78bf4fb4ea5f91
Keerthana Gb <keerthanagb14@gmail.com>
2026-06-06 22:37:46 +0530

    Fix CORS

--- api/index.py
+++ api/index.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import json
-import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
